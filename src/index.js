51824c4db908674c817e5965f9e3c14241ec16fa
leonluc-dev <leonlucardie@gmail.com>
2022-12-26 18:58:55 +0100

    Optimized code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-const tweetViewsSelector = "div.css-1dbjc4n.r-1mf7evn, div.css-1dbjc4n.r-18u37iz.r-1h0z5md";
+const viewCounterSingleSelector = "div.css-1dbjc4n.r-2sztyj.r-1efd50x.r-5kkj8d.r-13awgt0.r-18u37iz.r-tzz3ar.r-s1qlax.r-1yzf0co > div.css-1dbjc4n"; //View counter for: Single tweet without replies
+const viewCounterThreadSelector = "div.css-1dbjc4n.r-1mf7evn"; //View counter for: Single tweet in thread
+const viewCounterListSelector = "div.css-1dbjc4n.r-18u37iz.r-1h0z5md" //View counter for: Tweet in list/timeline
+
+const tweetViewsSelectors = [
+    viewCounterSingleSelector,
+    viewCounterThreadSelector,
+    viewCounterListSelector
+];
 
 let observer = new MutationObserver(mutations => {
-    const analyticElements = $("a[href*='/analytics']");
+    const allAddedNodes = mutations.flatMap(x=> Array.from(x.addedNodes ?? []));
+    //Find all view analytic links
+    const analyticElements = $(allAddedNodes).find("a[href*='/analytics']");
 
-	if(analyticElements) {
-		const viewElements = analyticElements.closest(tweetViewsSelector);
-		if(viewElements)
-			viewElements.remove();
-	}
+    if(analyticElements) {
+        //Find the closest parent views matching the viewcount selectors
+        const tweetViewElements = analyticElements.closest(tweetViewsSelectors.join(", "));
+        if(tweetViewElements) {
+            //Remove all viewcount elements in the list
+            tweetViewElements.remove();
+        }
+    }
 });
 observer.observe(document, { childList: true, subtree: true });
