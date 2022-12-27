40aa6387ea844a6d03c6f4b142140cb752cc8727
leonluc-dev <leonlucardie@gmail.com>
2022-12-27 14:09:43 +0100

    Added eslint, package.json and ran formatter

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "webextensions": true,
+        "commonjs": true,
+        "es2019": true,
+        "jquery": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
